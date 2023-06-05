db1ee59bce889a75298920849b3727865baa6f08
Kim jonghun <developerkimjh@gmail.com>
2023-06-05 13:47:12 +0900

    FIX: styled-component setting 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 
-import '@/styles/globals.css';
+import { GlobalStyle } from '@/styles/global-style';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
+      <GlobalStyle />
       <Component {...pageProps} />
     </>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,16 @@
-import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
     try {
@@ -11,16 +18,10 @@ export default class MyDocument extends Document {
         originalRenderPage({
           enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
         });
-      const initialProps = await Document.getInitialProps(ctx);
 
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
       };
     } finally {
       sheet.seal();
