d39a0d90388c9e664d56c98cd67f495a9156ba42
Kim jonghun <developerkimjh@gmail.com>
2023-06-06 16:27:29 +0900

    FIX: Header 전역으로 빼기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app';
 
 import { antdConfig } from '@/src/constants/antd';
 
+import Header from '@/src/components/Layout/Header/Header';
+
 import { GlobalStyle } from '@/src/styles/global-style';
 
 import { ConfigProvider } from 'antd';
@@ -10,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ConfigProvider theme={antdConfig}>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
     </ConfigProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import { SimplePost } from '@/src/constants/mockData';
+
+import SimplePostItem from '@/src/components/Items/SimplePostItem/SimplePostItem';
 import Header from '@/src/components/Layout/Header/Header';
 import MainSlider from '@/src/components/MainSlider/MainSlider';
 
 export default function Home() {
   return (
     <>
-      <Header />
       <MainSlider />
     </>
   );
