738f75b238e47b4236c879d60ef65c8730fb3441
Kim jonghun <developerkimjh@gmail.com>
2023-06-06 18:22:24 +0900

    FEAT: SimplePostItem 추가

---
+++ src/components/Items/SimplePostItem/SimplePostItem-styled.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 267px;
+  height: 150px;
+  padding: 24px;
+  border: 1px solid #849291;
+  border-radius: 18px;
+`;
+
+const TitleWrapper = styled.div`
+  font-size: 22px;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  height: 60px;
+`;
+
+const TagWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export { Container, TitleWrapper, TagWrapper };
---
+++ src/components/Items/SimplePostItem/SimplePostItem.tsx
@@ -0,0 +1,26 @@
+import Tag from '@/src/components/Tag/Tag';
+
+import * as S from './SimplePostItem-styled';
+
+//임시 items type
+interface SimplePostItemProps {
+  title: string;
+  tag: string[];
+}
+
+const SimplePostItem = (props: SimplePostItemProps) => {
+  const { title, tag } = props;
+
+  return (
+    <S.Container>
+      <S.TitleWrapper>{title}</S.TitleWrapper>
+      <S.TagWrapper>
+        {tag.map((v, i) => {
+          return <Tag key={i} text={v} />;
+        })}
+      </S.TagWrapper>
+    </S.Container>
+  );
+};
+
+export default SimplePostItem;
