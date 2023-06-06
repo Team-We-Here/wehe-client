200e11943ffbaf8bf77356d834047f72b1018085
Kim jonghun <developerkimjh@gmail.com>
2023-06-06 19:38:37 +0900

    FIX: import 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 
 import { antdConfig } from '@src/constants/antd';
 
-import Header from '@src/components/Layout/Header/Header';
+import Header from '@src/components/layout/Header/Header';
 
 import { GlobalStyle } from '@src/styles/global-style';
 
