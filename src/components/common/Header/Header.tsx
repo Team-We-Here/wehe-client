b92d44d53d00e9b923759fb9fbdedc9eb00f1311
peacemaker474 <hc456123@gmail.com>
2023-11-30 00:01:52 +0900

    feat: 헤더 영역 내 로그인 버튼 클릭 추가

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,17 +1,22 @@
 import "./Header.styles.scss";
 
+import Button from "../Button/Button";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <header className="common-header">
       <div className="common-header__container">
-        <Image
-          src="/icons/mainLogo.svg"
-          alt="wehe_main-logo"
-          width="144"
-          height="38"
-        />
+        <Link href="/">
+          <Image
+            src="/icons/mainLogo.svg"
+            alt="wehe_main-logo"
+            width="144"
+            height="38"
+          />
+        </Link>
+        <Button> 로그인 </Button>
       </div>
     </header>
   );
