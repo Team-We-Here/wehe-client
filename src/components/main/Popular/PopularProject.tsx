1eece47f073abd44e15b4c0d66586f80b1802949
peacemaker474 <hc456123@gmail.com>
2023-12-11 23:41:04 +0900

    feat: 조회수 및 댓글 아이콘 추가

--- src/components/main/Popular/PopularProject.tsx
+++ src/components/main/Popular/PopularProject.tsx
@@ -13,7 +13,7 @@ export default function PopularProject({ projectData }: any) {
         />
         <h3 className="m-0 p-[10px] text-[20px]"> 인기 프로젝트 </h3>
       </div>
-      <div className="grid-colos-5 gap-12">
+      <div className="grid grid-colos-5 grid-rows-[200px] gap-12">
         {projectData
           .filter((item: any) => item.authorNickname)
           .map((project: any) => (
